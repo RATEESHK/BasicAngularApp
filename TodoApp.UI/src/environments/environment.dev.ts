6c548cfd01d593907ae7070ce90f5530c17ed298
RATEESHK <rateesh8765@gmail.com>
2023-08-12 19:01:57 +0530

    Add environment config changes, missed in previous commit.

--- TodoApp.UI/src/environments/environment.dev.ts
+++ TodoApp.UI/src/environments/environment.dev.ts
@@ -1,6 +1,3 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 import { LogLevel, BrowserCacheLocation } from '@azure/msal-browser';
 
 const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
@@ -31,16 +28,13 @@ export const environment = {
         },
     },
     apiConfig: {
-        scopes:["api://xyz/xyz"],
+        scopes:{
+            read: ["api://xyz/ReadAccess"],
+            write: ["api://xyz/ReadAccess"],
+        },
         uri: "http://localhost:5184/api/"
+    },
+    loginRequest:{
+        scopes: []
     }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
